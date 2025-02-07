dfa6048db41ba2957371bdf4cb220fa1f833de85
Alexander E Genaud <alex@genaud.net>
2025-02-07 01:35:39 +0100

    Restricted Pali corrections

--- resources/bin/run_dirs_links_and_pali.sh
+++ resources/bin/run_dirs_links_and_pali.sh
@@ -2,21 +2,26 @@
 # bash resources/bin/run_dirs_links_and_pali.sh &
 
 echo Pāli corrections
-ls -d [dit]*.htm backmatter dhammatalk frontmatter
-ls -d dhamma-vinaya/bd dhamma-vinaya/pali
-echo Specific english and links corrections
+ls -d backmatter dhammatalk dhamma-vinaya/bd dhamma-vinaya/pali
+echo Specific English and links corrections
 ls -d [dit]*.htm backmatter dhamma* frontmatter
 
 RUN_PALI=resources/bin/run_file_pali.sh
-find [dit]*.htm backmatter dhammatalk frontmatter \
-  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 100 bash "$RUN_PALI"
-find dhamma-vinaya/bd dhamma-vinaya/pali \
-  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 100 bash "$RUN_PALI"
+find backmatter dhammatalk dhamma-vinaya/bd dhamma-vinaya/pali \
+  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 1000 bash "$RUN_PALI"
 
 RUN_ENGL=resources/bin/run_file_english.sh
 find [dit]*.htm backmatter dhamma* frontmatter \
-  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 100 bash "$RUN_ENGL"
+  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 1000 bash "$RUN_ENGL"
 
 RUN_LINK=resources/bin/run_file_link.sh
 find [dit]*.htm backmatter dhamma* frontmatter \
-  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 100 bash "$RUN_LINK"
+  -type f \( -name "*.htm" -o -name "*.txt" \) | xargs -n 1000 bash "$RUN_LINK"
+
+# root: No changes
+# frontmatter No changes
+# backmatter Can changed
+# dhammatalk Can change
+# dhamma-vinaya No change except
+# dhamma-vinaya/bd Can change
+# dhamma-vinaya/pali Can change
